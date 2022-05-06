eeae7846f9f39021832c4d6d43f290e597ec3870
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-05-06 18:14:15 +0200

    implementing home part1

--- src/main/java/com/techevents/techevents/controller/HomeController.java
+++ src/main/java/com/techevents/techevents/controller/HomeController.java
@@ -1,12 +1,36 @@
 package com.techevents.techevents.controller;
 
+import com.techevents.techevents.entity.Events;
+import com.techevents.techevents.service.IEventsService;
+import com.techevents.techevents.service.IUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private IEventsService eventsService;
+
+    @Autowired
+    private IUsersService usersService;
+
     @GetMapping({"/index", "/home", "/"})
-    public String index(){
+    public String index(Model model){
+        List<Events> listofEvents = eventsService.findAll();
+
+        model.addAttribute("title", "List of Events");
+        model.addAttribute("events", listofEvents);
+
+        List<Events> listadoDestacados = eventsService.listFeatured();
+        model.addAttribute("features", listOfFeatured);
+
         return "home";
     }
 }
