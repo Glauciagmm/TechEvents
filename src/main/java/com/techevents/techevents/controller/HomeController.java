2d8a58e08762d3541b6cc6fad4ee9b376dedfcbb
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-09 20:08:31 +0200

    pagination on main page

--- src/main/java/com/techevents/techevents/controller/HomeController.java
+++ src/main/java/com/techevents/techevents/controller/HomeController.java
@@ -1,15 +1,18 @@
 package com.techevents.techevents.controller;
 
 import com.techevents.techevents.entity.Events;
+import com.techevents.techevents.repository.EventsRepository;
 import com.techevents.techevents.service.IEventsService;
 import com.techevents.techevents.service.IUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -30,14 +33,32 @@ public class HomeController {
     private IEventsService eventsService;
 
     @Autowired
+    private EventsRepository eventsRepository;
+
+    @Autowired
     private IUsersService usersService;
 
     @GetMapping({"/index", "/home", "/"})
-    public String index(Model model, Authentication auth){
+    public String index(HttpServletRequest request, Model model, Authentication auth){
         List<Events> listadoEvents = eventsService.listarTodos();
 
         model.addAttribute("titulo", "Listado de Eventos");
-        model.addAttribute("events", listadoEvents);
+        model.addAttribute("", listadoEvents);
+
+        int page = 0; //default page number is 0 (yes it is weird)
+        int size = 3; //default page size is 10
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+        model.addAttribute("events", eventsRepository.findAll(PageRequest.of(page, size)));
+
+
 
         boolean isLoggedIn = false;
         if (auth != null){
